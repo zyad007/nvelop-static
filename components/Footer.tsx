808c388f64f046fc403a5cbb620f95557f5f9850
unknown <zyad.sallem.007@gmail.com>
2024-09-07 01:24:36 +0300

    zyad

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
-"use-client"
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='absolute w-[100%] flex justify-center text-center text-xs md:text-base md:h-20 h-16 items-center bg-[#0656FE] bottom-0 text-white'>
+    <footer className='absolute w-[100%] flex justify-center text-center text-xs md:text-base md:h-20 h-16 items-center bg-[#0656FE] bottom-0 text-white'>
         © Nvelop Technologies Oy. All rights reserved. Privacy Policy
-    </div>
+    </footer>
   )
 }
