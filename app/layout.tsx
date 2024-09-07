96c3906c7a541346e6392d41f88cd5ec4ad640b9
unknown <zyad.sallem.007@gmail.com>
2024-09-07 20:24:39 +0300

    Added Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Nvelop",
-  description: "Nvelop AI-powered IT sourcing"
+  abstract: 'AI-driven SaaS platform for sourcing ICT',
+  description: "Nvelop transforms the way software and Services are bought for large and mid sized organizations. Nvelop is a scalable, AI-native SaaS platform for sourcing IT services and software. Nvelop manages the entire sourcing process end-to-end, including automation of requirements gathering, proposal evaluation, contracting, and vendor management."
 };
 
 export default function RootLayout({
